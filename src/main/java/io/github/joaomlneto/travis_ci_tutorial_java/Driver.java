742e950e32ffaa066aaea598c6896e348e3f0e12
Amy Lewis <Amy Lewis>
2016-09-28 12:09:49 -0700

    Updated travis practice to follow driver instead of calculator

---
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/Driver.java
@@ -0,0 +1,12 @@
+package io.github.joaomlneto.travis_ci_tutorial_java;
+
+public class Driver {
+	public static void main(String[] args) {
+		int num = 2;
+		num *= 3;
+		num *= 7;
+		num *= 13;
+		num *= 37;
+		System.out.println(num);
+	}
+}
